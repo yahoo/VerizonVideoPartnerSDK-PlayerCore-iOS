f7a799fd45dc48655a0ffb34f7850db44dc5f261
RomanTysiachnik <roman.tysiachnik@verizonmedia.com>
2019-02-21 19:02:06 +0200

    Added ad progress in wrapper vast model

--- PlayerCoreTests/Components/VRMParsingResultComponentTest.swift
+++ PlayerCoreTests/Components/VRMParsingResultComponentTest.swift
@@ -11,6 +11,7 @@ class VRMParsingResultComponentTest: XCTestCase {
     
     let tag1URL = URL(string: "http://tag1.com")!
     let impression1 = URL(string:"http://impression1.com")!
+    let adProgress1 = Ad.VASTModel.AdProgress(url: URL(string: "http://test1")!, offset: .time(5))
     let adVerification1 = Ad.VASTModel.AdVerification(vendorKey: "vendorKey",
                                                       javaScriptResource: URL(string:"http://javaScriptResource1.com")!,
                                                       verificationParameters: nil,
@@ -18,12 +19,14 @@ class VRMParsingResultComponentTest: XCTestCase {
     
     let tag2URL = URL(string: "http://tag2.com")!
     let impression2 = URL(string:"http://impression2.com")!
+    let adProgress2 = Ad.VASTModel.AdProgress(url: URL(string: "http://test2")!, offset: .time(10))
     let adVerification2 = Ad.VASTModel.AdVerification(vendorKey: "vendorKey",
                                                       javaScriptResource: URL(string:"http://javaScriptResource2.com")!,
                                                       verificationParameters: nil,
                                                       verificationNotExecuted: nil)
     
     let impression3 = URL(string:"http://impression3.com")!
+    let adProgress3 = Ad.VASTModel.AdProgress(url: URL(string: "http://test3")!, offset: .percentage(32))
     let adVerification3 = Ad.VASTModel.AdVerification(vendorKey: "vendorKey",
                                                       javaScriptResource: URL(string:"http://javaScriptResource3.com")!,
                                                       verificationParameters: nil,
@@ -33,7 +36,8 @@ class VRMParsingResultComponentTest: XCTestCase {
         var sut = VRMParsingResult.initial
         let wrapper = VRMCore.VASTModel.WrapperModel(tagURL: tag1URL,
                                                      adVerifications: [adVerification1],
-                                                     pixels: AdPixels(impression: [impression1]))
+                                                     pixels: AdPixels(impression: [impression1]),
+                                                     adProgress: [adProgress1])
         
         let adVAST = Ad.VASTModel(adVerifications: [],
                                   mp4MediaFiles: [],
@@ -59,13 +63,15 @@ class VRMParsingResultComponentTest: XCTestCase {
     func testSecondWrapperForSameItem() {
         let wrapper = VRMCore.VASTModel.WrapperModel(tagURL: tag1URL,
                                                      adVerifications: [adVerification1],
-                                                     pixels: AdPixels(impression: [impression1]))
+                                                     pixels: AdPixels(impression: [impression1]),
+                                                     adProgress: [adProgress1])
         let initialResult = VRMParsingResult.Result(vastModel: .wrapper(wrapper))
         var sut = VRMParsingResult(parsedVASTs: [urlItem: initialResult])
         
         let secondWrapper = VRMCore.VASTModel.WrapperModel(tagURL: tag2URL,
                                                           adVerifications: [adVerification2],
-                                                          pixels: AdPixels(impression: [impression2]))
+                                                          pixels: AdPixels(impression: [impression2]),
+                                                          adProgress: [adProgress2])
         
         sut = reduce(state: sut, action: VRMCore.completeItemParsing(originalItem: urlItem,
                                                                      vastModel: .wrapper(secondWrapper)))
@@ -76,6 +82,7 @@ class VRMParsingResultComponentTest: XCTestCase {
             XCTAssertEqual(mergedWrapper.pixels.impression, [impression2, impression1])
             XCTAssertEqual(mergedWrapper.adVerifications, [adVerification2, adVerification1])
             XCTAssertEqual(mergedWrapper.tagURL, tag2URL)
+            XCTAssertEqual(mergedWrapper.adProgress, [adProgress2, adProgress1])
             
         } else {
             XCTFail()
@@ -85,7 +92,8 @@ class VRMParsingResultComponentTest: XCTestCase {
     func testInlineModelAfterWrappers() {
         let wrapper = VRMCore.VASTModel.WrapperModel(tagURL: tag1URL,
                                                      adVerifications: [adVerification2, adVerification1],
-                                                     pixels: AdPixels(impression: [impression2, impression1]))
+                                                     pixels: AdPixels(impression: [impression2, impression1]),
+                                                     adProgress: [adProgress2, adProgress1])
         
         let initialResult = VRMParsingResult.Result(vastModel: .wrapper(wrapper))
         var sut = VRMParsingResult(parsedVASTs: [urlItem: initialResult])
@@ -96,7 +104,7 @@ class VRMParsingResultComponentTest: XCTestCase {
                                   skipOffset: .none,
                                   clickthrough: nil,
                                   adParameters: nil,
-                                  adProgress: [],
+                                  adProgress: [adProgress3],
                                   pixels: AdPixels(impression: [impression3]),
                                   id: "")
         sut = reduce(state: sut, action: VRMCore.completeItemParsing(originalItem: urlItem,
@@ -107,6 +115,7 @@ class VRMParsingResultComponentTest: XCTestCase {
             XCTAssertNotEqual(result.id, initialResult.id)
             XCTAssertEqual(adModel.pixels.impression, [impression3, impression2, impression1])
             XCTAssertEqual(adModel.adVerifications, [adVerification3, adVerification2, adVerification1])
+            XCTAssertEqual(adModel.adProgress, [adProgress3, adProgress2, adProgress1])
             
         } else {
             XCTFail()
