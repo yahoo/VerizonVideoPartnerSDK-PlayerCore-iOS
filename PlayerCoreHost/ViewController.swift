5327f2ed3b14ec8abf59e4c375d564ac69b7c1fb
Yi-Chin Sun <yichin.sun@gmail.com>
2020-12-03 13:22:08 -0500

    Create Swift host target

---
+++ PlayerCoreHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerCoreHost
+//
+//  Created by Yi-Chin Sun on 12/3/20.
+//  Copyright © 2020 Oath Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
